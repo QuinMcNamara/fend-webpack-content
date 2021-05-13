e15a2dc71bd3fab30c2a32f1788fb60994f2a99b
Quin McNamara <81985894+QuinMcNamara@users.noreply.github.com>
2021-05-13 12:28:48 -0500

    Update index.js

--- src/client/index.js
+++ src/client/index.js
@@ -1,7 +1,15 @@
 import { handleSubmit } from './js/formHandler'
 import { checkForName } from './js/nameChecker'
 
+import './styles/resets.scss'
+import './styles/base.scss'
+import './styles/form.scss'
+import './styles/footer.scss'
+import './styles/header.scss'
+
 alert("I exist!")
 
-handleSubmit();
-checkForName();
+export {
+    handleSubmit,
+    checkForName
+}
