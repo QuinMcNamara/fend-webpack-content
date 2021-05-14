be26d3c4febdb9def3ff979716f52c271c099924
Quin McNamara <81985894+QuinMcNamara@users.noreply.github.com>
2021-05-14 16:37:13 -0500

    Update index.js

--- src/server/index.js
+++ src/server/index.js
@@ -3,25 +3,34 @@ dotenv.config();
 
 var path = require('path');
 const express = require('express');
-const mockAPIResponse = require('./mockAPI.js');
+const bodyParser = require('body-parser');
+const cors = require('cors');
 
-
+// Start up an instance of the app
 const app = express();
 
+// Initialize dist as project folder for app
 app.use(express.static('dist'));
 
-console.log(__dirname);
+app.use(express.urlencoded({extended: false}));
+app.use(express.json());
 
+// GET Route
 app.get('/', function (req, res) {
     res.sendFile('dist/index.html')
 });
 
-// designates what port the app will listen to for incoming requests
+// Designates what port the app will listen to for incoming requests
 const port = 8081;
-const server = app.listen(port, function () {
+const server = app.listen(port, function (error) {
+    if (error) console.log(error);
     console.log(`Example app listening on port: ${port}!`)
 });
 
+// Test GET Route
+const mockAPIResponse = require('./mockAPI.js');
 app.get('/test', function (req, res) {
     res.send(mockAPIResponse)
 });
+
+console.log(__dirname);
