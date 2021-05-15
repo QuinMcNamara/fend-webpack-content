6fe8cd719db906aeb507352740e5d6f2a1c809d1
Quin McNamara <81985894+QuinMcNamara@users.noreply.github.com>
2021-05-15 18:04:50 -0500

    Update formHandler.js
    Repeated code from earlier Weather App API Project to use as base for external API request in this project
    TO-DO: Refine into working code

--- src/client/js/formHandler.js
+++ src/client/js/formHandler.js
@@ -3,20 +3,74 @@ function handleSubmit(event) {
 
     // check what text was put into the form field
     let formText = document.getElementById('url').value
-    Client.isURL(formText)
+    
+    if (Client.isURL(formText)) {
 
-    console.log("::: Form Submitted :::")
-    fetch('http://localhost:8081/test')
-    .then(res => {
-        return res.json()
-    })
-    .then(function(data) {
-        document.getElementById('agreement').innerHTML = `Agreement: ${data.agreement}`;
-        document.getElementById('subjectivity').innerHTML = `Subjectivity: ${data.subjectivity}`;
-        document.getElementById('irony').innerHTML = `Irony: ${data.irony}`;
-        document.getElementById('confidence').innerHTML = `Confidence: ${data.confidence}`;
-        document.getElementById('polarity').innerHTML = `Polarity: ${data.polarity}`;
-    })
+        console.log("::: Form Submitted :::")
+        fetch('http://localhost:8081/test')
+        .then(res => {
+            return res.json()
+        })
+        .then(function(res) {
+            document.getElementById('agreement').innerHTML = `Agreement: ${res.agreement}`;
+            document.getElementById('subjectivity').innerHTML = `Subjectivity: ${res.subjectivity}`;
+            document.getElementById('irony').innerHTML = `Irony: ${res.irony}`;
+            document.getElementById('confidence').innerHTML = `Confidence: ${res.confidence}`;
+            document.getElementById('polarity').innerHTML = `Polarity: ${res.polarity}`;
+        })
+    }
+    else {
+        alert('The URL is invalid. Please enter a valid URL');
+    }
 }
+
+//Function to GET Web API Data
+const retrieveData = async(baseURL, zipCode, apiKey) => {
+	const response = await fetch(`${baseURL}zip=${zipCode}&appid=${apiKey}`)
+	try {
+		const allData = await response.json();
+		return allData;
+	}
+	catch(error) {
+		console.log('error:', error);
+	}
+};
+
+//Function to POST data
+const postData = async(url = '', data = {}) => {
+
+	const response = await fetch (url, {
+		method: 'POST',
+		credentials: 'same-origin',
+		headers: {
+			'Content-Type': 'application/json',
+		},
+		body: JSON.stringify(data),
+	});
+
+	try {
+		const newData = await response.json();
+		return newData;
+	}
+	catch(error) {
+		console.log('error:', error);
+	}
+};
+
+//Function to GET Project data and update UI
+// Modeled after code here: https://bithacker.dev/fetch-weather-openweathermap-api-javascript
+const updateUI = async() => {
+	const request = await fetch('/all');
+	try {
+		const weatherData = await request.json();
+		document.getElementById('date').innerHTML = `Date: ${weatherData.date}`;
+		document.getElementById('temp').innerHTML = `Current Temp: ${fahrenheit(weatherData.temp)}`;
+		document.getElementById('content').innerHTML = `Current Feeling: ${weatherData.content}`;
+	}
+	catch(error) {
+		console.log('error:', error);
+	}
+};
+
 
 export { handleSubmit };
