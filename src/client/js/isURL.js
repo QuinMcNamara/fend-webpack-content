bdefe2eee582f5666f15a4c86f1cbe695ca13838
Quin McNamara <81985894+QuinMcNamara@users.noreply.github.com>
2021-05-15 17:16:08 -0500

    Create isURL.js
    Function to check the validity of user input URL

---
+++ src/client/js/isURL.js
@@ -0,0 +1,21 @@
+/* Function to check validity of URL entered by user
+As seen here:
+https://www.w3resource.com/javascript-exercises/javascript-regexp-exercise-9.php
+*/
+
+function isURL(userURL)
+{
+  regexp =  /^(?:(?:https?|ftp):\/\/)?(?:(?!(?:10|127)(?:\.\d{1,3}){3})(?!(?:169\.254|192\.168)(?:\.\d{1,3}){2})(?!172\.(?:1[6-9]|2\d|3[0-1])(?:\.\d{1,3}){2})(?:[1-9]\d?|1\d\d|2[01]\d|22[0-3])(?:\.(?:1?\d{1,2}|2[0-4]\d|25[0-5])){2}(?:\.(?:[1-9]\d?|1\d\d|2[0-4]\d|25[0-4]))|(?:(?:[a-z\u00a1-\uffff0-9]-*)*[a-z\u00a1-\uffff0-9]+)(?:\.(?:[a-z\u00a1-\uffff0-9]-*)*[a-z\u00a1-\uffff0-9]+)*(?:\.(?:[a-z\u00a1-\uffff]{2,})))(?::\d{2,5})?(?:\/\S*)?$/;
+        if (regexp.test(userURL))
+        {
+          return true;
+        }
+        else
+        {
+          return false;
+        }
+}
+
+
+
+export { isURL };
