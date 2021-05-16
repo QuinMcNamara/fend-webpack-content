57c913dce982b4f2625ac00e1f91a2d145ecf513
Quin McNamara <81985894+QuinMcNamara@users.noreply.github.com>
2021-05-16 13:49:18 -0500

    Update index.js
    Adds POST Route for API

--- src/server/index.js
+++ src/server/index.js
@@ -15,6 +15,18 @@ app.use(express.urlencoded({extended: false}));
 app.use(express.json());
 app.use(cors());
 
+// Meaning Cloud API Variables
+const apiKey = process.env.API_KEY;
+const baseURL = 'https://api.meaningcloud.com/sentiment-2.1';
+let formEntry = '';
+
+// POST Route
+app.post('/api', async (req, res) => {
+    formEntry = req.body.url;
+    const response = await fetch(`${baseURL}?key=${apiKey}&url=${formEntry}&lang=en`);
+    const apiData = response.json;
+    res.send(apiData);
+});
 
 // GET Route
 app.get('/', function (req, res) {
@@ -23,7 +35,7 @@ app.get('/', function (req, res) {
 
 // Designates what port the app will listen to for incoming requests
 const port = 8081;
-const server = app.listen(port, function (error) {
+app.listen(port, function (error) {
     if (error) console.log(error);
     console.log(`Example app listening on port: ${port}!`)
 });
