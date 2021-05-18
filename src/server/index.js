1b30ea05841b3c2f81b128dfeec8d7da0558d3b4
Quin McNamara <81985894+QuinMcNamara@users.noreply.github.com>
2021-05-18 10:25:50 -0500

    Update index.js
    Creates new post route using example from sourced website

--- src/server/index.js
+++ src/server/index.js
@@ -1,11 +1,12 @@
-apiData = {};
-
+// Required to keep API key private
 const dotenv = require('dotenv');
 dotenv.config();
 
+// Dependencies
 var path = require('path');
 const express = require('express');
 const cors = require('cors');
+const fetch = require('node-fetch')
 
 // Start up an instance of the app
 const app = express();
@@ -13,36 +14,39 @@ const app = express();
 // Initialize dist as project folder for app
 app.use(express.static('dist'));
 
+// Replaces body-parser with Express => v4.16
 app.use(express.urlencoded({extended: false}));
 app.use(express.json());
+
 app.use(cors());
 
-// Meaning Cloud API Key
+// Meaning Cloud API Data
 const apiKey = process.env.API_KEY;
-let formEntry = '';
+const baseURL = 'https://api.meaningcloud.com/sentiment-2.1';
 
-// POST Route
-// app.post('/api', async (req, res) => {
-//     formEntry = req.body.url;
-//     const response = await fetch(`${baseURL}?key=${apiKey}&url=${formEntry}&lang=en`);
-//     try {
-//         apiData = response.json();
-//         console.log(apiData)
-//         res.send(apiData);
-//     }
-//     catch (error) {
-//         console.log('error', error)
-//     }
-// });
-
-// GET Routes
+// GET Route
 app.get('/', function (req, res) {
     res.sendFile('dist/index.html')
 });
 
-app.get('/apiInfo', function (req,res) {
-    res.send(apiKey)
-})
+// POST Route Using Node-Fetch as seen here:
+// https://stackabuse.com/making-http-requests-in-node-js-with-node-fetch/
+app.post('/meaningData', meaningData);
+
+// Function for POST Route
+function meaningData(req, res) {
+    const userURL = req.body.url;
+    fetch(`${baseURL}?key=${apiKey}&url=${userURL}&lang=en`, {
+        method: 'POST',
+		credentials: 'same-origin',
+		headers: {
+			'Content-Type': 'application/json',
+		},
+    })
+    .then(apiresponse => apiresponse.json())
+    .then(apiData => res.send(apiData))
+    .catch(err => console.log('Error with API Fetch', err));
+};
 
 // Designates what port the app will listen to for incoming requests
 const port = 8081;
